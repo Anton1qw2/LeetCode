6ae999b7fda3b580e8275b74ce2eabbd094adbca
Anton1qw2 <31093230+Anton1qw2@users.noreply.github.com>
2025-10-25 22:19:54 +0300

    Time: 0 ms (100.00%) | Memory: 8 MB (13.49%) - LeetSync

--- M/1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ M/1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -4,14 +4,12 @@ public:
         const auto [count_of_weeks, count_of_days] = std::div(n, 7);
         int money_per_week = 28;
         int money = 0;
-        for (int i = 0; i < count_of_weeks; i++) {
-            money += money_per_week;
-            money_per_week += 7;
-        }
+        //Sₙ = n/2 × (2a₁ + (n-1)×d)
+        money = count_of_weeks *
+                (2 * money_per_week + (count_of_weeks - 1) * 7) / 2;
+        money += count_of_days *
+                 (2 * (count_of_weeks + 1) + (count_of_days - 1)) / 2;
 
-        for (int i = 1; i <= count_of_days; i++) {
-            money += count_of_weeks + i;
-        }
         return money;
     }
 };
