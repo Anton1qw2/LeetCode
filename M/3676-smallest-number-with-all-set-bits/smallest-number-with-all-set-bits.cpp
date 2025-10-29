e0f8b41f18a3f340de37d2cf15411186c3ac230c
Anton1qw2 <31093230+Anton1qw2@users.noreply.github.com>
2025-10-29 17:29:27 +0300

    Time: 0 ms (100.00%) | Memory: 8.5 MB (11.95%) - LeetSync

--- M/3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ M/3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        return (n > 1) ? pow(2, std::ceil(std::log2(n + 1))) - 1 : 1;
+        return pow(2, std::ceil(std::log2(n + 1))) - 1;
     }
 };
