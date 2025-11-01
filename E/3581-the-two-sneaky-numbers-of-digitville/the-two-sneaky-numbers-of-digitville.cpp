aab1618157dbe1bc35288241c2466395bb1259b6
Anton1qw2 <31093230+Anton1qw2@users.noreply.github.com>
2025-11-01 18:11:08 +0300

    Time: 0 ms (100.00%) | Memory: 26.1 MB (58.23%) - LeetSync

---
+++ E/3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+        std::vector<int> result(2, 0);
+        int count_elements = 0;
+        std::set<int> unique_numbers;
+        for (auto& num : nums) {
+            const int prev_size = unique_numbers.size();
+            unique_numbers.insert(num);
+            if (prev_size == unique_numbers.size()) {
+                result[count_elements] = num;
+                count_elements++;
+                if (count_elements == 2)
+                    break;
+            }
+        }
+        return result;
+    }
+};
